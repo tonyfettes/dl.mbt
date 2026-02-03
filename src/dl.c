2fb3a6483abafcdb6bfddb71a44eaa1dd33fc3e5
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2026-02-03 17:39:13 +0800

    fix: modernize

--- src/dl.c
+++ src/dl.c
@@ -1,30 +1,30 @@
+#include <dlfcn.h>
 #include <moonbit.h>
+#include <string.h>
 
+MOONBIT_FFI_EXPORT
 void *
-moonbit_dlopen(moonbit_bytes_t filename, int flag) {
-  void *handle = dlopen((const char *)filename, flag);
-  printf("dlopen: %p\n", handle);
-  moonbit_decref(filename);
-  return handle;
+moonbit_dlopen(moonbit_bytes_t path, int32_t flags) {
+  return dlopen((const char *)path, flags);
 }
 
+MOONBIT_FFI_EXPORT
 void *
 moonbit_dlsym(void *handle, moonbit_bytes_t symbol) {
-  void *ptr = dlsym(handle, (const char *)symbol);
-  printf("dlsym: %p\n", ptr);
-  moonbit_decref(symbol);
-  return ptr;
+  return dlsym(handle, (const char *)symbol);
 }
 
+MOONBIT_FFI_EXPORT
 moonbit_bytes_t
 moonbit_dlerror() {
-  const char *error = dlerror();
-  size_t length = strlen(error);
-  moonbit_bytes_t bytes = moonbit_make_bytes(length, 0);
-  memcpy(bytes, error, length);
-  return bytes;
+  const char *src = dlerror();
+  size_t len = strlen(src);
+  moonbit_bytes_t dst = moonbit_make_bytes(len, 0);
+  memcpy(dst, src, len);
+  return dst;
 }
 
+MOONBIT_FFI_EXPORT
 void
 moonbit_dlclose(void *handle) {
   dlclose(handle);
